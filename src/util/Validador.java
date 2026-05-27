8e52d6c7fc69681a0cbd22cea9d5ce993f5eb465
diego281010 <diego.almeida03@epn.edu.ec>
2026-05-26 20:31:16 -0500

    util

--- src/util/Validador.java
+++ src/util/Validador.java
@@ -1,4 +1,28 @@
 package util;
 
 public class Validador {
+
+    public static boolean validarCorreo(String correo) {
+        return correo.contains("@") && correo.contains(".");
+    }
+
+    public static boolean validarTelefono(String telefono) {
+
+        for (int i = 0; i < telefono.length(); i++) {
+
+            if (!Character.isDigit(telefono.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean validarTexto(String texto) {
+        return !texto.trim().isEmpty();
+    }
+
+    public static boolean validarEdad(int edad) {
+        return edad > 0 && edad < 150;
+    }
 }
